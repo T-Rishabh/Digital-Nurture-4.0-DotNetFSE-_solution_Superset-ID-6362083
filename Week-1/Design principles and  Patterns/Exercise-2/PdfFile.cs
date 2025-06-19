ccff4a8f06fa18db860624016717271c42d7628f
T-Rishabh <shuklarishu036@gmail.com>
2025-06-19 21:35:26 +0530

    Added PdfFile.cs
    - contains class with custom Open and Close implementation

---
+++ Week-1/Design principles and  Patterns/Exercise-2/PdfFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class PdfFile : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("PDF file is now open.");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("PDF file has been closed.");
+    }
+}
