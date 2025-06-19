1838a854478530e29886dc2e406d15d0e5632554
T-Rishabh <shuklarishu036@gmail.com>
2025-06-19 21:40:07 +0530

    Created ExcelFile.cs
    -Contains class to handle Excel-specific document actions

---
+++ Week-1/Design principles and  Patterns/Exercise-2/ExcelFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ExcelFile : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Excel file is now open.");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("Excel file has been closed.");
+    }
+}
