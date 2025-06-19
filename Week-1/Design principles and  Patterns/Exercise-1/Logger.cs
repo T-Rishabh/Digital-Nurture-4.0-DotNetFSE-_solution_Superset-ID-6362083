e1ae50bc35babe1bfd4e7ac573e5796d9ae1dcca
T-Rishabh <shuklarishu036@gmail.com>
2025-06-19 20:01:30 +0530

    Updated code file in C#
    - removed previous file as it was in java
    - added  new program files in c#

---
+++ Week-1/Design principles and  Patterns/Exercise-1/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger instance;
+
+    private string user;
+    private string pass;
+
+    private Logger() { }
+ 
+    public static Logger GetLogger()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void SetCredentials(string username, string password)
+    {
+        user = username;
+        pass = password;
+    }
+
+    public void PrintCredentials()
+    {
+        Console.WriteLine("User: " + user);
+        Console.WriteLine("Pass: " + pass);
+    }
+}
---
+++ Week-1/Design principles and  Patterns/Exercise-1/LoggerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class LoggerTest
+{
+    public static void Main(string[] args)
+    {
+        Logger logA = Logger.GetLogger();
+        Logger logB = Logger.GetLogger();
+
+        if (logA == logB)
+        {
+            Console.WriteLine("Logger instances are identical.");
+        }
+        else
+        {
+            Console.WriteLine("Logger instances are different!");
+        }
+
+        Console.WriteLine();
+
+        RunCredentialCheck(logA, logB);
+    }
+
+    private static void RunCredentialCheck(Logger a, Logger b)
+    {
+        a.SetCredentials("adminUser", "pass123");
+        b.SetCredentials("guestUser", "pass456");
+
+        Console.WriteLine("Logger A credentials:");
+        a.PrintCredentials();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Logger B credentials:");
+        b.PrintCredentials();
+
+        Console.WriteLine("\nNote: Both loggers reflect the same state due to Singleton behavior.");
+    }
+}
