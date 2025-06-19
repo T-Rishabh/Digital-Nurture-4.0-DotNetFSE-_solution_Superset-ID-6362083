69df7fe2e9644679f13f0b152d5f46f6bb88b748
T-Rishabh <shuklarishu036@gmail.com>
2025-06-19 21:54:46 +0530

    Added Program.cs
    - main test class to simulate document creation using factory method

---
+++ Week-1/Design principles and  Patterns/Exercise-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        RunDocumentTest("Word");
+        RunDocumentTest("PDF");
+        RunDocumentTest("Excel");
+    }
+
+    public static void RunDocumentTest(string type)
+    {
+        DocumentCreator creator = null;
+
+        switch (type.ToLower())
+        {
+            case "word":
+                creator = new WordCreator();
+                break;
+            case "pdf":
+                creator = new PdfCreator();
+                break;
+            case "excel":
+                creator = new ExcelCreator();
+                break;
+            default:
+                Console.WriteLine("Unsupported document type.");
+                return;
+        }
+
+        IDocument document = creator.Create();
+        Console.WriteLine($"\n[TEST] Creating and working with a {type} document:");
+        document.Open();
+        document.Close();
+    }
+}
