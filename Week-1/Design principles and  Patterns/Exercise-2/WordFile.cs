734550268a5ef7eee2a44d034b76d73447e86536
T-Rishabh <shuklarishu036@gmail.com>
2025-06-19 21:32:54 +0530

    Created WordFile.cs
    - contains class implementing IDocument interface

---
+++ Week-1/Design principles and  Patterns/Exercise-2/WordFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class WordFile : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Word file is now open.");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("Word file has been closed.");
+    }
+}
