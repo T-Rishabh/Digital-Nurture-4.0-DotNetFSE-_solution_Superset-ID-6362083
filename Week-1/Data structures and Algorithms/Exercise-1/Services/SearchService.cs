c19a1524b7ba544b6e2351cb98ce22e19714e21d
T-Rishabh <shuklarishu036@gmail.com>
2025-06-22 00:41:24 +0530

    Implement SearchService.cs inside Servise folder
    - it implements contains linear and binary search algorithms
    - Enables search functionality based on ProductId
    - Designed for reusability within the product search workflow

---
+++ Week-1/Data structures and Algorithms/Exercise-1/Services/SearchService.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceProductSearch.Models;
+
+namespace ECommerceProductSearch.Services
+{
+    public class SearchService
+    {
+        public static Product FindByLinearSearch(Product[] items, int targetId)
+        {
+            foreach (var item in items)
+            {
+                if (item.ProductId == targetId)
+                    return item;
+            }
+            return null;
+        }
+
+        public static Product FindByBinarySearch(Product[] items, int targetId)
+        {
+            int left = 0, right = items.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (items[mid].ProductId == targetId)
+                    return items[mid];
+                else if (items[mid].ProductId < targetId)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return null;
+        }
+    }
+}
