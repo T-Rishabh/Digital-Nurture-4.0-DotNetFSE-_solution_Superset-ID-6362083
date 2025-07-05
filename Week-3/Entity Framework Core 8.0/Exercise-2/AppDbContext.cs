e549932c1cf68ccfba8c19fdcf01e4bcc9361fc6
T-Rishabh <shuklarishu036@gmail.com>
2025-07-05 18:22:57 +0530

    Add Category and Product entity classes

---
+++ Week-3/Entity Framework Core 8.0/Exercise-2/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Your_Connection_String_Here");
+    }
+}
