3218742b50566e6a98df41c85f666f129c7936b7
T-Rishabh <shuklarishu036@gmail.com>
2025-07-13 02:15:13 +0530

    Add ValuesController.cs
    - Add CustomAuthFilter to validate Authorization header and Bearer token

---
+++ Week-4/ASP.NET Core 8.0 Web API/Exercise-3/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Rishabh", "Zoie" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        [HttpGet("throw")]
+        public IActionResult ThrowException()
+        {
+            throw new Exception("Test exception");
+        }
+
+    }
+}
