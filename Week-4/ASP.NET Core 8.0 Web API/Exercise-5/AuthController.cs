0a4651b6c92525679c3895420118ff44d856733f
T-Rishabh <shuklarishu036@gmail.com>
2025-07-13 19:52:20 +0530

    update AuthController.cs
    - implement AuthController to generate JWT with claims and expiration

---
+++ Week-4/ASP.NET Core 8.0 Web API/Exercise-5/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("token")]
+        public IActionResult GetToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecretkey1234567890abcd"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim("UserId", "1")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "mySystem",
+                audience: "myUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(2), 
+                signingCredentials: credentials);
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+    }
+}
