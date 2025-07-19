14df3ca52b1b703e1bc1aba14575223c12a4cfae
T-Rishabh <shuklarishu036@gmail.com>
2025-07-19 22:35:43 +0530

    Added Contollers
    - implement login endpoint generating JWT with role claim from config
    - add protected endpoint requiring JWT authorization

---
+++ Week-5/Microservices Architecture using ASP.NET Core Web API/Controllers/SecureController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SecureController : ControllerBase
+{
+    [HttpGet("data")]
+    [Authorize]
+    public IActionResult GetSecureData()
+    {
+        return Ok("This is protected data.");
+    }
+}
+
+[ApiController]
+[Route("api/[controller]")]
+public class AdminController : ControllerBase
+{
+    [HttpGet("dashboard")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetAdminDashboard()
+    {
+        return Ok("Welcome to the admin dashboard.");
+    }
+}
