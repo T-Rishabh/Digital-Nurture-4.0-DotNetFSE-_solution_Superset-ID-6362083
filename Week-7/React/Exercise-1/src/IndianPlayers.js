da49043418b8003fbc433391293d1a7938efbde7
T-Rishabh <shuklarishu036@gmail.com>
2025-08-02 23:53:50 +0530

    added src/IndianPlayers.js
    - Add IndianPlayers component using merge and destructuring

---
+++ Week-7/React/Exercise-1/src/IndianPlayers.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+
+function IndianPlayers() {
+    const T20players = ['Virat', 'Rohit', 'Rishabh', 'Hardik'];
+    const RanjiTrophy = ['Pujara', 'Rahane', 'Karun Nair', 'Saha'];
+
+   
+    const allPlayers = [...T20players, ...RanjiTrophy];
+
+   
+    const [player1, player2, player3, player4, ...rest] = allPlayers;
+
+  
+    const oddPlayers = [player2, player4]; 
+    const evenPlayers = [player1, player3]; 
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <h2>Odd Team Players (Destructured)</h2>
+            <ul>
+                {oddPlayers.map((p, i) => <li key={i}>{p}</li>)}
+            </ul>
+
+            <h2>Even Team Players (Destructured)</h2>
+            <ul>
+                {evenPlayers.map((p, i) => <li key={i}>{p}</li>)}
+            </ul>
+
+            <h2>Merged Players</h2>
+            <ul>
+                {allPlayers.map((p, i) => <li key={i}>{p}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+export default IndianPlayers;
