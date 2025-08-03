8d278c928d9aeed98d0d4f952bba9f3503700481
T-Rishabh <shuklarishu036@gmail.com>
2025-08-03 14:52:44 +0530

    git add src/GuestPage.js
    Add GuestPage with styled flight listings for guest view

---
+++ Week-7/React/Exercise-4/src/GuestPage.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+function GuestPage() {
+    return (
+        <div>
+            <h2 style={{ color: '#34495e' }}>Welcome, Guest 👋</h2>
+            <p>Please login to book your tickets.</p>
+            <ul style={{ lineHeight: '1.8' }}>
+                <li>✈️ Flight A - ₹5000</li>
+                <li>✈️ Flight B - ₹7000</li>
+            </ul>
+        </div>
+    );
+}
+
+export default GuestPage;
