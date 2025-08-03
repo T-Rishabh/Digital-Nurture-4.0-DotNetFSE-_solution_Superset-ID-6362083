b33e57f61ac364516a4d67418a810b8757e08ff9
T-Rishabh <shuklarishu036@gmail.com>
2025-08-03 15:22:10 +0530

    added src/CourseDetails.js
    - Add CourseDetails component showing courses with duration

---
+++ Week-7/React/Exercise-5/src/CourseDetails.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+
+function CourseDetails() {
+    const courses = [
+        { id: 'r1', name: 'ReactJS Fundamentals', duration: '4 weeks' },
+        { id: 'j1', name: 'JavaScript Essentials', duration: '6 weeks' },
+    ];
+
+    return (
+        <div>
+            <h3>🎓 Courses Offered</h3>
+            <ul>
+                {courses.map(course => (
+                    <li key={course.id}>
+                        {course.name} - {course.duration}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default CourseDetails;
