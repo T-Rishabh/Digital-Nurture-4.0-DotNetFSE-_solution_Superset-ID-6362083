ca119a80bceae06b4bc6cc7f77f80d969fe11bbe
T-Rishabh <shuklarishu036@gmail.com>
2025-08-03 15:20:43 +0530

    added src/BlogDetails.js
    - Add BlogDetails component with blog list and unique keys

---
+++ Week-7/React/Exercise-5/src/BlogDetails.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+
+function BlogDetails() {
+    const blogs = [
+        { id: 1, title: 'React Tips', date: '2025-08-01' },
+        { id: 2, title: 'JavaScript Tricks', date: '2025-07-20' },
+    ];
+
+    return (
+        <div>
+            <h3>📝 Blog Posts</h3>
+            <ul>
+                {blogs.map(({ id, title, date }) => (
+                    <li key={id}>
+                        <strong>{title}</strong> - <em>{date}</em>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default BlogDetails;
