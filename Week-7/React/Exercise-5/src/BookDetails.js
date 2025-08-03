555b149f3a52e429b522ac9e7fe9fc63f1a9d34c
T-Rishabh <shuklarishu036@gmail.com>
2025-08-03 15:19:14 +0530

    added src/BookDetails.js
    - Add BookDetails component with book list using map() and key

---
+++ Week-7/React/Exercise-5/src/BookDetails.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+
+function BookDetails() {
+    const books = [
+        { id: 1, title: 'Clean Code', author: 'Robert C. Martin' },
+        { id: 2, title: 'The Pragmatic Programmer', author: 'Andrew Hunt' },
+    ];
+
+    return (
+        <div>
+            <h3>📚 Book List</h3>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>
+                        <strong>{book.title}</strong> by {book.author}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default BookDetails;
